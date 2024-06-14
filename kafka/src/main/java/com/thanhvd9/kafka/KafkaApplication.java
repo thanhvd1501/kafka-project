920f17666859e815dff5397a67a9b840cef42957
thanhvd9 <vuducthanh0115@gmail.com>
2024-06-14 19:53:47 +0700

    [BE][kafka]:create kafka project

---
+++ kafka/src/main/java/com/thanhvd9/kafka/KafkaApplication.java
@@ -0,0 +1,13 @@
+package com.thanhvd9.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApplication.class, args);
+	}
+
+}
